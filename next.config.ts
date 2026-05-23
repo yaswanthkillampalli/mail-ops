e4d5422897980477f175c95859d98195d0a6e3a9
yaswanthkillampalli <yaswanthkillampalli@gmail.com>
2026-05-24 02:31:30 +0530

    feat: update allowedDevOrigins to include mail-ops.yashcodes.tech

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["email-tp.yashcodes.tech"],
+  allowedDevOrigins: ["email-tp.yashcodes.tech","mail-ops.yashcodes.tech"],
 };
 
 export default nextConfig;
